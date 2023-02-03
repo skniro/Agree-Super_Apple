208ff25df95fd35753b022d0f37f0dfb48bac55b
skniro <46392259+skniro@users.noreply.github.com>
2023-02-03 13:43:46 +0800

    Add EFFICIENCY level 6-10

--- src/main/java/com/skniro/agree/mixin/Mixin_EfficiencyEnchantment.java
+++ src/main/java/com/skniro/agree/mixin/Mixin_EfficiencyEnchantment.java
@@ -6,6 +6,6 @@ import org.spongepowered.asm.mixin.Mixin;
 @Mixin(EfficiencyEnchantment.class)
 public class Mixin_EfficiencyEnchantment {
     public int getMaxLevel() {
-        return 5;
+        return 10;
     }
 }
