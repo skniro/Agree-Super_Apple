ea165581ae8fed738cf49be1f3a17d6ce1dd885b
skniro <46392259+skniro@users.noreply.github.com>
2023-02-11 22:03:14 +0800

    Fix all mixin Enchantment inject.

--- src/main/java/com/skniro/agree/mixin/Mixin_LureEnchantment.java
+++ src/main/java/com/skniro/agree/mixin/Mixin_LureEnchantment.java
@@ -2,10 +2,14 @@ package com.skniro.agree.mixin;
 
 import net.minecraft.enchantment.LureEnchantment;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LureEnchantment.class)
 public class Mixin_LureEnchantment {
-    public int getMaxLevel() {
-        return 5;
+    @Inject(method = "getMaxLevel", at = @At("RETURN"), cancellable = true)
+    public void maxLevel(CallbackInfoReturnable<Integer> cir){
+        cir.setReturnValue(5);
     }
 }
--- src/main/java/com/skniro/agree/mixin/Mixin_PunchEnchantment.java
+++ src/main/java/com/skniro/agree/mixin/Mixin_PunchEnchantment.java
@@ -2,10 +2,14 @@ package com.skniro.agree.mixin;
 
 import net.minecraft.enchantment.PunchEnchantment;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PunchEnchantment.class)
 public class Mixin_PunchEnchantment {
-    public int getMaxLevel() {
-        return 4;
+    @Inject(method = "getMaxLevel", at = @At("RETURN"), cancellable = true)
+    public void maxLevel(CallbackInfoReturnable<Integer> cir){
+        cir.setReturnValue(4);
     }
 }
