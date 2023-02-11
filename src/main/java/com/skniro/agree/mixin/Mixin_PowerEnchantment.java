0fe5c173162c2598d3c784d6e8d25f2bfe94b593
skniro <46392259+skniro@users.noreply.github.com>
2023-02-11 22:13:15 +0800

    Fix all mixin Enchantment inject.

--- src/main/java/com/skniro/agree/mixin/Mixin_PowerEnchantment.java
+++ src/main/java/com/skniro/agree/mixin/Mixin_PowerEnchantment.java
@@ -2,10 +2,14 @@ package com.skniro.agree.mixin;
 
 import net.minecraft.enchantment.PowerEnchantment;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PowerEnchantment.class)
 public class Mixin_PowerEnchantment {
-    public int getMaxLevel() {
-        return 8;
+    @Inject(method = "getMaxLevel", at = @At("RETURN"), cancellable = true)
+    public void maxLevel(CallbackInfoReturnable<Integer> cir){
+        cir.setReturnValue(8);
     }
 }
