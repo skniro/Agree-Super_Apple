b103b833a87211f2b0e3dbc7eb7e4c056e979d86
skniro <46392259+skniro@users.noreply.github.com>
2023-02-11 22:04:40 +0800

    Fix all mixin Enchantment inject.

--- src/main/java/com/skniro/agree/mixin/Mixin_SweepingEnchantment.java
+++ src/main/java/com/skniro/agree/mixin/Mixin_SweepingEnchantment.java
@@ -2,10 +2,14 @@ package com.skniro.agree.mixin;
 
 import net.minecraft.enchantment.SweepingEnchantment;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SweepingEnchantment.class)
 public class Mixin_SweepingEnchantment {
-    public int getMaxLevel() {
-        return 5;
+    @Inject(method = "getMaxLevel", at = @At("RETURN"), cancellable = true)
+    public void maxLevel(CallbackInfoReturnable<Integer> cir){
+        cir.setReturnValue(5);
     }
 }
