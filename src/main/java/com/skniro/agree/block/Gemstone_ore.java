a0199114d8bb808d7eee0441ef3f9c6a9df867e7
skniro <46392259+skniro@users.noreply.github.com>
2023-07-17 18:26:07 +0800

    1.14.4-1.9.0

--- src/main/java/com/skniro/agree/block/Gemstone_ore.java
+++ src/main/java/com/skniro/agree/block/Gemstone_ore.java
@@ -3,7 +3,6 @@ package com.skniro.agree.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.block.OreBlock;
-import net.minecraft.sound.BlockSoundGroup;
 
 public class Gemstone_ore {
     public static final Block RUBY_ORE = new OreBlock(Block.Settings.of(Material.STONE).strength(3.0F, 3.0F));
