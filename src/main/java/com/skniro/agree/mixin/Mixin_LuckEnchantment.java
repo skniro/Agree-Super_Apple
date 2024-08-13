953321e25a6113a4d3fffe3e3f6576c8060ce6dc
skniro <46392259+skniro@users.noreply.github.com>
2024-08-13 19:07:13 +0800

    Fix crash issue and recipes.

--- src/main/java/com/skniro/agree/mixin/Mixin_LuckEnchantment.java
+++ src/main/java/com/skniro/agree/mixin/Mixin_LuckEnchantment.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LootBonusEnchantment.class)
 public class Mixin_LuckEnchantment {
-    @Inject(method = "getMaxLevel", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "getMaxLevel()I", at = @At("HEAD"), cancellable = true)
     public void maxLevel(CallbackInfoReturnable<Integer> cir){
         if(AgreeConfig.Enchantment_Module.get()) {
             cir.setReturnValue(5);
