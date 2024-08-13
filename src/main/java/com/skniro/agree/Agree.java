c6457dcbe289f762bcfc494fc5adf6175c8d7574
skniro <46392259+skniro@users.noreply.github.com>
2024-08-13 19:34:29 +0800

    Fix recipes and crash issue.

--- src/main/java/com/skniro/agree/Agree.java
+++ src/main/java/com/skniro/agree/Agree.java
@@ -42,7 +42,7 @@ public class Agree {
 
         // Register the Deferred Register to the mod event bus so blocks get registered
         AppleFoodComponents.registerModItems(modEventBus);
-        //AgreeRecipeSerializer.agreerecipeseroalizer(modEventBus);
+        AgreeRecipeSerializer.agreerecipeseroalizer(modEventBus);
         Gemstone.registerModItems(modEventBus);
         AgreeItems.registerModItems(modEventBus);
         AgreeBlocks.registerAgreeBlocks(modEventBus);
