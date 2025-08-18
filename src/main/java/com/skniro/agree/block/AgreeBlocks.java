f1e1cde22bf9024e9384009ddcd7cc4aa5ae8dee
skniro <46392259+skniro@users.noreply.github.com>
2025-08-18 17:38:31 +0800

    Function:shears with dispensers in order to collect the apples.  Fix: The age of the crop will not return to 0 after harvesting.  Adjust: Make LeafCropBlock.fruitItem public.

--- src/main/java/com/skniro/agree/block/AgreeBlocks.java
+++ src/main/java/com/skniro/agree/block/AgreeBlocks.java
@@ -43,24 +43,24 @@ public class AgreeBlocks {
             ()-> new SaplingBlock(JumpBoostAppleSaplingGenerator.JumpBoostAppleSapling, BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_SAPLING)), ModCreativeModeTabs.Agree_Group);
 
     //LEAVES
-    public static final RegistryObject<Block> HASTE_APPLE_LEAVES =registerBlock("haste_apple_leave",
-            ()-> new LeafCropBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GREEN), AppleFoodComponents.HASTE_APPLE),ModCreativeModeTabs.Agree_Group);
-    public static final RegistryObject<Block> SPEED_APPLE_LEAVES =registerBlock("speed_apple_leave",
-            ()-> new LeafCropBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GREEN), AppleFoodComponents.SPEED_APPLE),ModCreativeModeTabs.Agree_Group);
-    public static final RegistryObject<Block> HEALTH_BOOST_LEAVES =registerBlock("health_boost_leave",
-            ()-> new LeafCropBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GREEN), AppleFoodComponents.HEALTH_BOOST_APPLE),ModCreativeModeTabs.Agree_Group);
-    public static final RegistryObject<Block> FIRE_RESISTANCE_LEAVES =registerBlock("fire_resistance_leave",
-            ()-> new LeafCropBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GREEN), AppleFoodComponents.FIRE_RESISTANCE_APPLE),ModCreativeModeTabs.Agree_Group);
-    public static final RegistryObject<Block> HERO_VILLAGE_LEAVES =registerBlock("village_hero_leave",
-            ()-> new LeafCropBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GREEN), AppleFoodComponents.HERO_VILLAGE_APPLE),ModCreativeModeTabs.Agree_Group);
-    public static final RegistryObject<Block> STRENGTH_LEAVES =registerBlock("strength_apple_leave",
-            ()-> new LeafCropBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GREEN), AppleFoodComponents.STRENGTH_APPLE),ModCreativeModeTabs.Agree_Group);
-    public static final RegistryObject<Block> NIGHT_VISION_LEAVES =registerBlock("night_vision_leave",
-            ()-> new LeafCropBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GREEN), AppleFoodComponents.NIGHT_VISION_APPLE),ModCreativeModeTabs.Agree_Group);
-    public static final RegistryObject<Block> JUMP_BOOST_LEAVES =registerBlock("jump_boost_leave",
-            ()-> new LeafCropBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GREEN), AppleFoodComponents.JUMP_BOOST_APPLE), ModCreativeModeTabs.Agree_Group);
-    public static final RegistryObject<Block> Apple_Tree_LEAVES =registerBlock("apple_tree_leave",
-            ()-> new LeavesBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GREEN)),ModCreativeModeTabs.Agree_Group);
+    public static final Supplier<Block> HASTE_APPLE_LEAVES =registerBlock("haste_apple_leave",
+            ()-> new LeafCropBlock(BlockBehaviour.Properties.of().noOcclusion().mapColor(MapColor.COLOR_GREEN), AppleFoodComponents.HASTE_APPLE),ModCreativeModeTabs.Agree_Group);
+    public static final Supplier<Block> SPEED_APPLE_LEAVES =registerBlock("speed_apple_leave",
+            ()-> new LeafCropBlock(BlockBehaviour.Properties.of().noOcclusion().mapColor(MapColor.COLOR_GREEN), AppleFoodComponents.SPEED_APPLE),ModCreativeModeTabs.Agree_Group);
+    public static final Supplier<Block> HEALTH_BOOST_LEAVES =registerBlock("health_boost_leave",
+            ()-> new LeafCropBlock(BlockBehaviour.Properties.of().noOcclusion().mapColor(MapColor.COLOR_GREEN), AppleFoodComponents.HEALTH_BOOST_APPLE),ModCreativeModeTabs.Agree_Group);
+    public static final Supplier<Block> FIRE_RESISTANCE_LEAVES =registerBlock("fire_resistance_leave",
+            ()-> new LeafCropBlock(BlockBehaviour.Properties.of().noOcclusion().mapColor(MapColor.COLOR_GREEN), AppleFoodComponents.FIRE_RESISTANCE_APPLE),ModCreativeModeTabs.Agree_Group);
+    public static final Supplier<Block> HERO_VILLAGE_LEAVES =registerBlock("village_hero_leave",
+            ()-> new LeafCropBlock(BlockBehaviour.Properties.of().noOcclusion().mapColor(MapColor.COLOR_GREEN), AppleFoodComponents.HERO_VILLAGE_APPLE),ModCreativeModeTabs.Agree_Group);
+    public static final Supplier<Block> STRENGTH_LEAVES =registerBlock("strength_apple_leave",
+            ()-> new LeafCropBlock(BlockBehaviour.Properties.of().noOcclusion().mapColor(MapColor.COLOR_GREEN), AppleFoodComponents.STRENGTH_APPLE),ModCreativeModeTabs.Agree_Group);
+    public static final Supplier<Block> NIGHT_VISION_LEAVES =registerBlock("night_vision_leave",
+            ()-> new LeafCropBlock(BlockBehaviour.Properties.of().noOcclusion().mapColor(MapColor.COLOR_GREEN), AppleFoodComponents.NIGHT_VISION_APPLE),ModCreativeModeTabs.Agree_Group);
+    public static final Supplier<Block> JUMP_BOOST_LEAVES =registerBlock("jump_boost_leave",
+            ()-> new LeafCropBlock(BlockBehaviour.Properties.of().noOcclusion().mapColor(MapColor.COLOR_GREEN), AppleFoodComponents.JUMP_BOOST_APPLE), ModCreativeModeTabs.Agree_Group);
+    public static final Supplier<Block> Apple_Tree_LEAVES =registerBlock("apple_tree_leave",
+            ()-> new LeavesBlock(BlockBehaviour.Properties.of().noOcclusion().mapColor(MapColor.COLOR_GREEN)),ModCreativeModeTabs.Agree_Group);
 
     //Potted Plant
     public static final RegistryObject<Block> POTTED_HASTE_APPLE_SAPLING = registerBlockWithoutItem("potted_haste_apple_sapling",
--- src/main/java/com/skniro/agree/client/AgreeClient.java
+++ src/main/java/com/skniro/agree/client/AgreeClient.java
@@ -2,39 +2,46 @@ package com.skniro.agree.client;
 
 import com.skniro.agree.Agree;
 import com.skniro.agree.block.AgreeBlocks;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 
-public class AgreeClient{
-    /*@Override
-    public void onInitializeClient() {
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.HASTE_APPLE_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.SPEED_APPLE_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.STRENGTH_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.HEALTH_BOOST_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.FIRE_RESISTANCE_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.HERO_VILLAGE_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.NIGHT_VISION_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.JUMP_BOOST_SAPLING, RenderLayer.getCutout());
+@Mod.EventBusSubscriber(modid = Agree.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class AgreeClient {
+    @SuppressWarnings({"deprecation","removal"})
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.HASTE_APPLE_SAPLING.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.SPEED_APPLE_SAPLING.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.STRENGTH_SAPLING.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.HEALTH_BOOST_SAPLING.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.FIRE_RESISTANCE_SAPLING.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.HERO_VILLAGE_SAPLING.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.NIGHT_VISION_SAPLING.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.JUMP_BOOST_SAPLING.get(), RenderType.cutout());
 
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.POTTED_HASTE_APPLE_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.POTTED_SPEED_APPLE_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.POTTED_STRENGTH_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.POTTED_HEALTH_BOOST_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.POTTED_FIRE_RESISTANCE_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.POTTED_HERO_VILLAGE_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.POTTED_NIGHT_VISION_SAPLING, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.POTTED_JUMP_BOOST_SAPLING, RenderLayer.getCutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.POTTED_HASTE_APPLE_SAPLING.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.POTTED_SPEED_APPLE_SAPLING.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.POTTED_STRENGTH_SAPLING.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.POTTED_HEALTH_BOOST_SAPLING.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.POTTED_FIRE_RESISTANCE_SAPLING.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.POTTED_HERO_VILLAGE_SAPLING.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.POTTED_NIGHT_VISION_SAPLING.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.POTTED_JUMP_BOOST_SAPLING.get(), RenderType.cutout());
 
 
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.HASTE_APPLE_LEAVES, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.SPEED_APPLE_LEAVES, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.STRENGTH_LEAVES, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.HEALTH_BOOST_LEAVES, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.FIRE_RESISTANCE_LEAVES, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.HERO_VILLAGE_LEAVES, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.NIGHT_VISION_LEAVES, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.JUMP_BOOST_LEAVES, RenderLayer.getCutout());
-        BlockRenderLayerMap.INSTANCE.putBlock(AgreeBlocks.Apple_Tree_LEAVES, RenderLayer.getCutout());
-
-    }*/
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.HASTE_APPLE_LEAVES.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.SPEED_APPLE_LEAVES.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.STRENGTH_LEAVES.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.HEALTH_BOOST_LEAVES.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.FIRE_RESISTANCE_LEAVES.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.HERO_VILLAGE_LEAVES.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.NIGHT_VISION_LEAVES.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.JUMP_BOOST_LEAVES.get(), RenderType.cutout());
+        ItemBlockRenderTypes.setRenderLayer(AgreeBlocks.Apple_Tree_LEAVES.get(), RenderType.cutout());
+    }
 }
