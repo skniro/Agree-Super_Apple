2317e30b802c73a167e2c3da3d96dacfa5f8d88e
skniro <46392259+skniro@users.noreply.github.com>
2025-11-08 17:57:15 +0800

    Fix GoldenAppleDispenserBehaviors overrides vanilla shear function.

--- src/main/java/com/skniro/agree/Agree.java
+++ src/main/java/com/skniro/agree/Agree.java
@@ -5,7 +5,6 @@ import com.skniro.agree.Enchantment.EnchantmentModule;
 import com.skniro.agree.block.AgreeBlocks;
 import com.skniro.agree.block.Gemstone_ore;
 import com.skniro.agree.conifg.AgreeConfig;
-import com.skniro.agree.event.GoldenAppleDispenserBehaviors;
 import com.skniro.agree.item.AgreeItems;
 import com.skniro.agree.item.Apples.AppleFoodComponents;
 import com.skniro.agree.item.Gemstone;
@@ -56,7 +55,6 @@ public class Agree {
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-        GoldenAppleDispenserBehaviors.register();
     }
 
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
