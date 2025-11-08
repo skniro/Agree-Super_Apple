e500e1c2278a4555df6acb93a72df70aa701b17e
skniro <46392259+skniro@users.noreply.github.com>
2025-11-08 17:19:39 +0800

    Trying fix GoldenAppleDispenserBehaviors overrides vanilla shear function.

--- Fabric/src/main/java/com/skniro/agree/event/GoldenAppleDispenserBehaviors.java
+++ Fabric/src/main/java/com/skniro/agree/event/GoldenAppleDispenserBehaviors.java
@@ -1,9 +1,11 @@
 package com.skniro.agree.event;
 
+
 import com.skniro.agree.block.init.LeafCropBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.DispenserBlock;
+import net.minecraft.block.dispenser.DispenserBehavior;
 import net.minecraft.block.dispenser.FallibleItemDispenserBehavior;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
@@ -20,10 +22,17 @@ import net.minecraft.world.event.GameEvent;
 public class GoldenAppleDispenserBehaviors {
 
     public static void register() {
-        DispenserBlock.registerBehavior(Items.SHEARS, new ShearsDispenserBehavior());
+        var oldBehavior = DispenserBlock.BEHAVIORS.get(Items.SHEARS);
+        DispenserBlock.registerBehavior(Items.SHEARS, new ShearsDispenserBehavior(oldBehavior));
     }
 
     private static class ShearsDispenserBehavior extends FallibleItemDispenserBehavior {
+        private final DispenserBehavior original;
+
+        public ShearsDispenserBehavior(DispenserBehavior original) {
+            this.original = original;
+        }
+
         protected ItemStack dispenseSilently(BlockPointer pointer, ItemStack stack) {
             ServerWorld serverWorld = pointer.world();
             if (!serverWorld.isClient()) {
@@ -35,7 +44,7 @@ public class GoldenAppleDispenserBehaviors {
                 }
             }
 
-            return stack;
+            return original != null ? original.dispense(pointer, stack) : super.dispenseSilently(pointer, stack);
         }
 
         private static boolean tryShearBlock(ServerWorld world, BlockPos pos) {
--- Neoforge/src/main/java/com/skniro/agree/event/GoldenAppleDispenserBehaviors.java
+++ Neoforge/src/main/java/com/skniro/agree/event/GoldenAppleDispenserBehaviors.java
@@ -1,10 +1,10 @@
 package com.skniro.agree.event;
 
-
 import com.skniro.agree.block.init.LeafCropBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.dispenser.BlockSource;
 import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.core.dispenser.DispenseItemBehavior;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -17,10 +17,17 @@ import net.minecraft.world.level.gameevent.GameEvent;
 
 public class GoldenAppleDispenserBehaviors {
     public static void register() {
-        DispenserBlock.registerBehavior(Items.SHEARS, new ShearsDispenserBehavior());
+        var oldBehavior = DispenserBlock.DISPENSER_REGISTRY.get(Items.SHEARS);
+        DispenserBlock.registerBehavior(Items.SHEARS, new ShearsDispenserBehavior(oldBehavior));
     }
 
     private static class ShearsDispenserBehavior extends DefaultDispenseItemBehavior {
+        private final DispenseItemBehavior original;
+
+        public ShearsDispenserBehavior(DispenseItemBehavior original) {
+            this.original = original;
+        }
+
         @Override
         protected ItemStack execute(BlockSource source, ItemStack stack) {
             ServerLevel world = source.level();
@@ -29,7 +36,7 @@ public class GoldenAppleDispenserBehaviors {
             if (tryShearBlock(world, targetPos, state, stack)) {
                 return stack;
             }
-            return super.execute(source, stack);
+            return original != null ? original.dispense(source, stack) : super.execute(source, stack);
         }
 
         private static boolean tryShearBlock(ServerLevel world, BlockPos pos, BlockState state, ItemStack stack) {
